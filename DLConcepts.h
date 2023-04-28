b4b298ca574d7f878c1aa7329f01f3c4884fa394
TDennisCS <60369869+TDennisCS@users.noreply.github.com>
2023-04-28 02:33:43 +0000

    edited perceptron comments

--- DLConcepts.h
+++ DLConcepts.h
@@ -3,25 +3,21 @@
 #include<algorithm>
 #include<DLMath.h>
 
+// not using namespace std remember!!! 
 
 
-
-template <typename T> class Perceptron // y = g(w + XW)
+template <typename T> class Perceptron // y = g(w0 + sum of the dot poducts of (x, w))
 {
     private:
-        std::vector<T> weights; // weights 
-        std::vector<T> inputs; // Inputs
-        T bias; // w 
-        T activationFunction; // g 
-        T output; // y
+        std::vector <std::vector<T>> weights; // list of weight vectors  --> w
+        std::vector <std::vector<T>> inputs; // list of Input vectors --> x
+        T bias; // bias --> w0
+        T activationFunction; // activation function --> g  
+        T output; //  output of the perceptron --> y 
+        virtual Calcalute();
 
     public:
-        Perceptron(std::vector<T> weightsVector, std::vector<T> inputsVector, T bias, T activationFuntion);
-        T CalcaluteOutput(); 
+        Perceptron(std::vector<std::vector<T>> weightsVector, std::vector<std::vector<T>> inputsVector, T bias, T activationFuntion);
         T Output();
-
-
-
-
 };
 
