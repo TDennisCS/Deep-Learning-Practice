48d1f6af4948a1d3c78db15998d5404d3a866631
TDennisCS <60369869+TDennisCS@users.noreply.github.com>
2023-04-28 16:14:46 +0000

    switched to using functions as the building blocks

--- DLConcepts.h
+++ DLConcepts.h
@@ -1,23 +1,10 @@
 #include<vector>
-#include<iostream>
 #include<algorithm>
 #include<DLMath.h>
 
 // not using namespace std remember!!! 
 
 
-template <typename T> class Perceptron // y = g(w0 + sum of the dot poducts of (x, w))
-{
-    private:
-        std::vector <std::vector<T>> weights; // list of weight vectors  --> w
-        std::vector <std::vector<T>> inputs; // list of Input vectors --> x
-        T bias; // bias --> w0
-        T activationFunction; // activation function --> g  
-        T output; //  output of the perceptron --> y 
-        virtual Calcalute();
+template <typename Domain> Domain Perceptron (std::vector<Domain> weightsVector, std::vector<Domain> inputsVector, Domain bias, Domain activationFuntion);
 
-    public:
-        Perceptron(std::vector<std::vector<T>> weightsVector, std::vector<std::vector<T>> inputsVector, T bias, T activationFuntion);
-        T Output();
-};
-
+template <typename Domain> Domain LossOptimization (std::vector<Domain> weightsVector, std::vector<Domain> InputsVector, Domain ouptut,  int i, int n); 
