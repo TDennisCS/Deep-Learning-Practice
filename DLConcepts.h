ef5cb6cfb6f62872d5f1500c497a5e128a81c643
TDennisCS <60369869+TDennisCS@users.noreply.github.com>
2023-04-28 16:50:43 +0000

    O

--- DLConcepts.h
+++ DLConcepts.h
@@ -5,8 +5,9 @@
 // not using namespace std remember!!! 
 
 
-template <typename Domain> Domain Perceptron (std::vector<Domain> weightsVector, std::vector<Domain> inputsVector, Domain bias, Domain activationFuntion, int layers); // y = g(z), z = w0 + X * W
+template <typename Domain> Domain Perceptron (std::vector<Domain> weightsVector, std::vector<Domain> inputsVector, Domain bias, Domain activationFuntion); // y = g(z), z = w0 + X * W
  
 template <typename Domain> Domain LossOptimization (std::vector<Domain> weightsVector, std::vector<Domain> InputsVector, Domain ouptut, int layers);
 
 template<typename Domain> Domain BackPropagation(std::vector<Domain> weightsVector, std::vector<Domain> InputsVector, Domain loss, Domain output, int layers);
+
